6c657c645e8e39d2633196e9f3e0a567a4739e2a
Remillardj <jaryd.remillard@gmail.com>
2017-12-21 14:53:45 -0800

    add SCCM installation script

---
+++ install_sccm.ps1
@@ -0,0 +1,13 @@
+﻿$CREDENTIALS = ""
+$SPLASH_HTA_PATH = ""
+
+if (Test-Path "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe") {
+    Start-Process "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe" -Credential $CREDENTIALS        
+} else {
+    if (Test-Path "C:\Windows\ccmsetup") {
+        cd 'C:\Windows\ccmsetup'
+        ccmsetup.exe /uninstall
+        ccmsetup.exe /remove
+    }
+    Start-Process $SPLASH_HTA_PATH
+}
